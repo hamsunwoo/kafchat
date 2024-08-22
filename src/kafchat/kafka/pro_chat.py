f0ac5c34f18339b40f91e165c64f254332063136
sunwoo <summerham22@gmail.com>
2024-08-22 11:57:10 +0900

    AWS 원격으로 메세지 보내기

--- src/kafchat/kafka/pro_chat.py
+++ src/kafchat/kafka/pro_chat.py
@@ -2,8 +2,9 @@ from kafka import KafkaProducer
 import json
 import time
 
+
 p = KafkaProducer(
-        bootstrap_servers=['localhost:9092'],
+        bootstrap_servers=['ec2-43-203-219-5.ap-northeast-2.compute.amazonaws.com:9092'],
         value_serializer=lambda x: json.dumps(x).encode('utf-8')
 )
 
@@ -16,7 +17,8 @@ while True:
         break
 
     data = {'message': msg, 'time': time.time()}
-    p.send('chat', value=data)
+    p.send('chat-group', value=data)
+
     p.flush()
 
 print("채팅 종료")
